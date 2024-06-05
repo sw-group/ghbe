efb573d15b6861b02760095e591f93740e46f9b6
v-orlando <vittoriaorlando217@gmail.com>
2024-06-05 16:27:21 +0200

    Fix mapping data issues. Fix deserialized date.

--- swagger_server/utils/mapper.py
+++ swagger_server/utils/mapper.py
@@ -10,9 +10,9 @@ def map_response_to_repository(response):
         url=response.get('url'),
         private=response.get('private', False),
         language=response.get('language'),
-        created_at=util.deserialize_datetime(response['created_at']) if 'created_at' in response else None,
-        updated_at=util.deserialize_datetime(response['updated_at']) if 'updated_at' in response else None,
-        pushed_at=util.deserialize_datetime(response['pushed_at']) if 'pushed_at' in response else None,
+        created_at=util.deserialize_datetime(response['created_at']) if response.get('created_at') else None,
+        updated_at=util.deserialize_datetime(response['updated_at']) if response.get('updated_at') else None,
+        pushed_at=util.deserialize_datetime(response['pushed_at']) if response.get('pushed_at') else None,
         stars_count=response.get('stars_count'),
         forks_count=response.get('forks_count'),
         watchers_count=response.get('watchers_count'),
@@ -29,16 +29,17 @@ def map_response_to_workflow(response):
         path=response.get('path'),
         url=response.get('url'),
         state=response.get('state'),
-        created_at=util.deserialize_datetime(response['created_at']) if 'created_at' in response else None,
-        updated_at=util.deserialize_datetime(response['updated_at']) if 'updated_at' in response else None,
+        created_at=util.deserialize_datetime(response['created_at']) if response.get('created_at') else None,
+        updated_at=util.deserialize_datetime(response['updated_at']) if response.get('updated_at') else None,
         file_url=response.get('html_url')  # Assuming 'html_url' contains the file URL
     )
 
-def map_response_to_issue(issue):
+
+def map_response_to_issue(repo, issue):
     """Maps a dictionary from MongoDB to an Issue instance."""
     return Issue(
         number=issue.get('number'),
-        repo=issue.get('repo'),
+        repo=repo,
         title=issue.get('title'),
         url=issue.get('url'),
         state=issue.get('state'),
@@ -46,15 +47,16 @@ def map_response_to_issue(issue):
         author=issue.get('author'),
         labels=map_issue_labels(dict(issue.get('labels', []))),
         comments=map_issue_comments(dict(issue.get('comments', {}))) if issue.get('comments') else None,
-        created_at=util.deserialize_datetime(issue['createdAt']) if 'createdAt' in issue else None,
-        updated_at=util.deserialize_datetime(issue['updatedAt']) if 'updatedAt' in issue else None,
-        # TODO: error when null
-        #closed_at=util.deserialize_datetime(issue['closedAt']) if 'closedAt' in issue else None
+        created_at=util.deserialize_datetime(issue.get('createdAt')) if issue.get('createdAt') else None,
+        updated_at=util.deserialize_datetime(issue.get('updatedAt')) if issue.get('updatedAt') else None,
+        closed_at=util.deserialize_datetime(issue.get('closedAt')) if issue.get('closedAt') else None,
     )
+
 
 def map_issue_labels(label_dicts):
     """Maps a list of dictionaries to a list of IssueLabels instances."""
     return [IssueLabels.from_dict(label) for label in label_dicts.get('nodes')]
+
 
 def map_issue_comments(comment_dict):
     """Maps a dictionary to an IssueComments instance."""
@@ -63,10 +65,11 @@ def map_issue_comments(comment_dict):
         list=[map_comment_info(dict(comment)) for comment in comment_dict.get('nodes')]
     )
 
+
 def map_comment_info(comment):
     return Comment(
         author=comment.get('author'),
         message=comment.get('body'),
-        created_at=util.deserialize_datetime(comment.get('createdAt')) if 'createdAt' in comment else None,
-        updated_at=util.deserialize_datetime(comment['updatedAt']) if 'updatedAt' in comment else None
+        created_at=util.deserialize_datetime(comment.get('createdAt')) if comment.get('createdAt') else None,
+        updated_at=util.deserialize_datetime(comment['updatedAt']) if comment.get('updatedAt') else None
     )
